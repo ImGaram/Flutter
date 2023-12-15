6018d6f5d86255e9858c0ed24204033e91a6a85f
imgaram <gim78014@gmail.com>
2023-12-15 10:52:26 +0900

    flutter basic 1: box

---
+++ flutter_basic/android/app/src/main/kotlin/com/example/flutterBasic/flutter_basic/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterBasic.flutter_basic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_basic/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+              '앱임',
+            style: TextStyle(
+              color: Colors.white
+            ),
+          ),
+          backgroundColor: Colors.blue,
+        ),
+        body: Align(
+          alignment: Alignment.bottomLeft,
+          child: Container(
+            // double.infinity: kotlin의 match_parent와 같은 기능
+            // margin 주는 방법: EdgeInsets.all(20)
+            // decoration: box의 여러 장식 추가 가능
+            // border, color, shape 설정 등등 가능
+            width: double.infinity, height: 50, margin: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              border: Border.all(color: Colors.black),
+              shape: BoxShape.rectangle
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
